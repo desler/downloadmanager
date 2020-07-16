06901ec900d7673eed91276c935b73cf2b9de67a
IT-shendaojun <497902218@qq.com>
2020-07-16 14:50:04 +0800

    DownloadManager 添加 taskmanager ,方便 对 下载任务进行管理， 因为需要 额外的一些计算及内存资源，此功能 默认不开取。需要使用此功能，请先调用 enableTaskManager

--- app/src/main/java/com/avit/downloadmanager/executor/AbsExecutor.java
+++ app/src/main/java/com/avit/downloadmanager/executor/AbsExecutor.java
@@ -1,9 +1,97 @@
 package com.avit.downloadmanager.executor;
 
+import androidx.core.util.Pair;
+
+import com.avit.downloadmanager.task.ITask;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.FutureTask;
 
 public abstract class AbsExecutor implements Callable<Boolean> {
+
+    private ConcurrentHashMap<String, Pair<ITask, FutureTask<Boolean>>> futureTasks;
+
+    public AbsExecutor() {
+        futureTasks = new ConcurrentHashMap<>();
+    }
+
+    protected Pair<ITask, FutureTask<Boolean>> putIfAbsent(String key, Pair<ITask, FutureTask<Boolean>> value){
+        return futureTasks.putIfAbsent(key, value);
+    }
+
+    public boolean isFinish(ITask task){
+
+        String key = task.getDownloadItem().getKey();
+
+        Pair<ITask, FutureTask<Boolean>> pair = futureTasks.get(key);
+        FutureTask<Boolean> futureTask = pair.second;
+        if (futureTask == null || futureTask.isDone() || futureTask.isCancelled()){
+            futureTasks.remove(key);
+            return true;
+        }
+
+        return false;
+    }
+
+    public ITask.State getTaskState(ITask task){
+        String key = task.getDownloadItem().getKey();
+
+        Pair<ITask, FutureTask<Boolean>> pair = futureTasks.get(key);
+        FutureTask<Boolean> futureTask = pair.second;
+        if (futureTask == null || futureTask.isDone() || futureTask.isCancelled()){
+            futureTasks.remove(key);
+            return ITask.State.NONE;
+        }
+
+        ITask t = pair.first;
+        if (t != null){
+            return t.getState();
+        }
+
+        return task.getState();
+    }
+
+    public final boolean isExist(ITask task){
+        return futureTasks.containsKey(task.getDownloadItem().getKey());
+    }
+
+    @Override
+    public Boolean call() throws Exception {
+
+        Set<Map.Entry<String, Pair<ITask, FutureTask<Boolean>>>> entrySet = futureTasks.entrySet();
+        Iterator<Map.Entry<String, Pair<ITask, FutureTask<Boolean>>>> iterator = entrySet.iterator();
+
+        for (; iterator.hasNext(); ) {
+            Map.Entry<String, Pair<ITask, FutureTask<Boolean>>> entry = iterator.next();
+            Pair<ITask, FutureTask<Boolean>> pair = entry.getValue();
+            FutureTask<Boolean> task = pair.second;
+            if (task == null || task.isDone() || task.isCancelled()){
+                iterator.remove();
+            }
+        }
+
+        return true;
+    }
+
     public void release(){
 
+        Enumeration<String> enumeration = futureTasks.keys();
+
+        while (enumeration.hasMoreElements()){
+            String key = enumeration.nextElement();
+            FutureTask<Boolean> task = futureTasks.get(key).second;
+
+            if (task == null || task.isDone() || task.isCancelled())
+                continue;
+
+            task.cancel(true);
+        }
+
+        futureTasks.clear();
     }
 }
